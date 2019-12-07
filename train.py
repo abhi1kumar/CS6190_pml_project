874751748c481794f601cb1c4e50967e44e2c41f
aishgupta <aish322@gmail.com>
2019-12-06 23:14:13 -0700

    updated E-step and partially M-step

--- train.py
+++ train.py
@@ -20,11 +20,9 @@ import torch.nn.functional as F
 # ============================================================
 lambda_w  = 1.0
 lambda_si = 1.0
+r = 5 
 
-
-
-
-def E_step(U, V, Beta, r=0):
+def E_step(U, V, Beta, M, r):
 	"""
 		Calculates the E step of the algorithm
 		
@@ -38,11 +36,10 @@ def E_step(U, V, Beta, r=0):
 		tau   = shape Ls x L
 	"""
 	zheta = torch.matmul(U, torch.transpose(V, 0, 1)) # N x Ls
-	omega = 1/(2*zheta) * F.tanh(zheta/2)             # N x Ls
+	omega = (1/(2*zheta)) * F.tanh(zheta/2)             # N x Ls
 	
 	gamma = torch.matmul(V, torch.transpose(Beta, 0, 1)) # Ls x L
-	M = torch.zeros(gamma.shape)
-	tau = (M +r)/ (2*gamma) * F.tanh(gamma/2)
+	tau = ((M +r)/ (2*gamma)) * F.tanh(gamma/2)
 
 	return omega, tau
 
@@ -60,14 +57,47 @@ def ridge_solver_batch(Sigma, Vector):
 	#output = torch.zeros((Vector.shape[0], Sigma.shape[1]))
 
 	# Add a new dimension so that matrix multiplication could be applied
-	Vector = Vector.view[:,:, :]
+	Vector = Vector[:, :, None]
 
 	# Inverse works in batches
 	# https://pytorch.org/docs/stable/torch.html#torch.inverse
 	Sigma_inverse = torch.inverse(Sigma)
-	output = torch.bmm(Sigma_inverse, Vector)
+	output = torch.bmm(Sigma_inverse, Vector).squeeze()
 	
 	return output
+
+def update_W(X, U, lambda_w):
+
+	# Update for W
+	Sigma_w = torch.matmul(torch.transpose(X, 0, 1), X) + lambda_w * torch.eye(X.shape[1]).type(X.type())
+	W = torch.inverse(Sigma_w) * torch.matmul(torch.transpose(X, 0, 1), U)
+	
+	return W
+
+def update_U(X, Y, U, V, W, omega):
+	"""
+		Calculates the updated U for the M step of the EM algorithm
+
+		Inputs:
+		X    = shape N  x D
+		V    = shape Ls x K
+		U    = shape N  x K
+		Beta = shape L  x K
+		Y    = shape N  x Ls 
+		omega= shape N x L
+
+		Output:
+		U    = shape N x K
+	"""
+
+	# Update for U
+    Y = Y - 0.5
+    sigma_U = torch.empty(V.shape[0], V.shape[1], V.shape[1])
+    v1 = V[:, :, None]
+    v2 = V[:, None, :]
+    for i in range(U.shape[0]):
+        omega_i = (omega[i])[:, None, None].repeat(1, v.shape[1], v.shape[1])
+        sigma_u[i] = (torch.sum(omega_i*torch.matmul(v1,v2), axis=0)+torch.eye(v.shape[1])).inverse()
 
 def M_step(X, U, V, Beta):
 	"""
@@ -76,6 +106,7 @@ def M_step(X, U, V, Beta):
 		Inputs:
 		X    = shape N  x D
 		V    = shape Ls x K
+		U    = shape N  x K
 		Beta = shape L  x K
 
 		Output:
@@ -83,7 +114,4 @@ def M_step(X, U, V, Beta):
 	"""
 	# Five update equations
 
-	# Update for W
-	Sigma_w = torch.matmul(torch.transpose(X, 0, 1), X) + lambda_w * torch.eye(X.shape[1]).type(X.type())
-	W_t = torch.inverse(Sigma_w) * torch.transpose(X, 0, 1) * U
-	W = W.transpose(0, 1)
+	
