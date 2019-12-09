8bc24277f40bbe1236c5b12a60e6d6a5eacd950b
abhi1kumar <abhinav3663@gmail.com>
2019-12-08 21:12:19 -0700

    minor changes

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ import argparse
 import os.path as osp
 import numpy as np
 
-def read_datafile(dir_path, data_file_rel_path):
+def get_data_labels_from_datafile(dir_path, data_file_rel_path):
     """
         Reads data file
     """
@@ -15,7 +15,6 @@ def read_datafile(dir_path, data_file_rel_path):
         for idx, line in enumerate(f.readlines()):
             if(idx==0):
                 N, D, L = [int(k) for k in line.split(' ')]
-                print(N, D, L)
                 X = np.zeros((N, D))
                 Y = np.zeros((N, L))
             else:
@@ -25,37 +24,21 @@ def read_datafile(dir_path, data_file_rel_path):
                 for xval_info in temp[1:]:
                     x_idx, x_val = xval_info.split(':')
                     X[idx-1][int(x_idx)] = float(x_val)
+    print("Data   X shape =", X.shape)
+    print("Labels Y shape =", Y.shape)
+
     return X, Y
 
-def read_splitfile(dir_path, filename):
+def get_indices_for_different_splits(dir_path, split_file_rel_path):
     """
-        Returns split file index
+        Returns arrays of each of the training splits.
+        The indices are in the columns of the file.
     """
-    split = np.array([])
-    with open(osp.join(dir_path, filename), 'r') as f:
-        for line in f.readlines():
-            temp = [int(k) for k in line.split(' ')]
-            if(split.size==0):
-                split = np.array(temp).reshape(-1,1)
-            else:
-                temp = np.array(temp).reshape(-1,1)
-                split = np.hstack((split,temp))
-    # Split is zero indexed
-    split -= 1  
-    return split
+    split_file_full_path = osp.join(dir_path, split_file_rel_path)
+    print("Reading {}...".format(split_file_full_path))
+    
+    indices_for_each_split = np.genfromtxt(split_file_full_path, delimiter= " ").T
+    # Indices should be zero indexed
+    indices_for_each_split  -= 1
 
-def train_model(X, Y, split):
-
-    train_X = X[split,:]
-    train_Y = Y[split,:]
-    EM_algorithm(iterations, train_X, train_Y, V, U, M, W, beta, lambda_u, lambda_v, lambda_beta, lambda_w, r)
-
-def test_data_sanity(X, Y, idx):
-
-    print("X shape : ", X.shape,"\nY shape : ", Y.shape, "\n")
-    feat_index = np.arange(X.shape[1])
-    label_index = np.arange(Y.shape[1])
-    print("Sparse representation of X[",idx,"] : \n", feat_index[X[idx]==1],"\n")
-    print("Sparse representation of Y[", idx, "] : \n", label_index[Y[idx]==1],"\n")
-    print("Number of ones in X[{}] : {} \n".format(idx, np.sum(X[idx])))
-    print("Number of ones in Y[{}] : {} \n".format(idx, np.sum(Y[idx])))
+    return indices_for_each_split
